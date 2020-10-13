d778bddef204acbe303ca8fee66de7119177d942
Miki Ashkenazi <mikiash@gmail.com>
2020-10-13 17:49:06 +0300

    app component & module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { WorkersComponent } from './workers/workers.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WorkersComponent
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     AppRoutingModule,
     BrowserAnimationsModule
   ],
