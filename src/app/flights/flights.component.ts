70eeda60ef97f323b5a5dc09cee51127ff5e6fc4
Miki Ashkenazi <mikiash@gmail.com>
2020-10-13 21:01:29 +0300

    workerSelected behaviour subject

--- src/app/flights/flights.component.ts
+++ src/app/flights/flights.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { DataService } from '../services/data.service';
+import { Flight } from '../interfaces/flight';
+import { Observable, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-flights',
   templateUrl: './flights.component.html',
   styleUrls: ['./flights.component.scss']
 })
-export class FlightsComponent implements OnInit {
-
-  constructor() { }
+export class FlightsComponent implements OnInit, OnDestroy {
+  flights$: Observable<Flight[]>;
+  sub: Subscription;
+  displayedColumns: string[] = ['flightNumber', 'origin', 'originDate', 'destination', 'destinationDate'];
+  constructor(private dataService: DataService) {}
 
   ngOnInit(): void {
+    this.sub = this.dataService.workerSelected$.subscribe(workerId => {
+      this.flights$ = this.dataService.getFlights();
+    });
   }
 
+  ngOnDestroy() {
+    this.sub.unsubscribe();
+  }
 }
--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -1,8 +1,9 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from '../../environments/environment';
-import { Observable } from 'rxjs';
+import { Observable, BehaviorSubject } from 'rxjs';
 import { Worker } from '../interfaces/worker';
+import { Flight } from '../interfaces/flight';
 
 @Injectable({
   providedIn: 'root',
@@ -10,6 +11,7 @@ import { Worker } from '../interfaces/worker';
 export class DataService {
   private baseUrl: string;
   private _selectedWorker: number;
+  workerSelected$: BehaviorSubject<number> = new BehaviorSubject(undefined);
 
   constructor(private http: HttpClient) {
     this.baseUrl = environment.baseUrl;
@@ -17,6 +19,7 @@ export class DataService {
 
   public set selectedWorker(v: number) {
     this._selectedWorker = v;
+    this.workerSelected$.next(v);
   }
 
   public get selectedWorker(): number {
@@ -27,5 +30,11 @@ export class DataService {
     const route = 'workers';
     const url = new URL(route, this.baseUrl).toString();
     return this.http.get<Worker[]>(url);
+  }
+
+  getFlights(): Observable<Flight[]> {
+    const route = `workers/${this.selectedWorker}`;
+    const url = new URL(route, this.baseUrl).toString();
+    return this.http.get<Flight[]>(url);
   }
 }
